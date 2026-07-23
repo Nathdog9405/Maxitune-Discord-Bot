e24e94c5a61658a7d6a3da7df7f8df3f6ded72aa
Nathdog9405 <duongnathan9405@gmail.com>
2026-07-23 20:27:35 +1000

    Add initial bot setup with environment variable loading and logging

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the Discord token from the environment variables
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
